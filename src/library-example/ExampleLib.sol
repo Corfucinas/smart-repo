43cfede096f4b1e05fee5a52aaeb5e66dad3f676
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-10-03 15:49:08 +0800

    add library examples

---
+++ src/library-example/ExampleLib.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.8.0;
+// pragma experimental ABIEncoderV2;
+// pragma experimental SMTChecker;
+
+library ExampleLib {
+    // meant to be used inside a smart-contract
+    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        return c;
+    }
+
+}
---
+++ src/library-example/ImportExample.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.8.0;
+// pragma experimental ABIEncoderV2;
+// pragma experimental SMTChecker;
+
+import "./ExampleLib.sol";
+
+contract Import {
+    using ExampleLib for uint256;
+    uint256 public value;
+
+    function calculate(uint _value1, uint _value2) public {
+        // value = Math.divide(_value1, _value2); this can be used if it's on the same contract
+        value = _value1.divide(_value2);
+    }
+}
