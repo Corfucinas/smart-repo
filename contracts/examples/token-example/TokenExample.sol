ddcf453894b97190a5460a9397d2937153480bae
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-10-06 00:01:50 +0800

    bump the compilter to 0.7.2

--- contracts/examples/token-example/TokenExample.sol
+++ contracts/examples/token-example/TokenExample.sol
@@ -2,11 +2,11 @@
 
 // Need to add a rec. function not just fallback()
 
-pragma solidity >=0.7.0 <0.8.0;
+pragma solidity = 0.7.2;
 // pragma experimental ABIEncoderV2;
 // pragma experimental SMTChecker;
 
-contract ERC20Token {
+contract ERC20TokenExample {
     // in order to talk to this smart contract we need to know the address
     string public name;
     mapping(address => uint256) public balances;
@@ -24,13 +24,13 @@ contract ERC20Token {
 }
 
 contract MyToken is
-    ERC20Token //inheritance, no need to double deploy
+    ERC20TokenExample //inheritance, no need to double deploy
 {
     string public symbol; // take parameters from the parent contract
     // string public name = "My token";  //overwrite the parent functionality
     address[] public owners;
     uint256 owner_count;
-    constructor(string memory _name, string memory _symbol) ERC20Token(_name) {
+    constructor(string memory _name, string memory _symbol) ERC20TokenExample(_name) {
         symbol = _symbol;
 
     }
