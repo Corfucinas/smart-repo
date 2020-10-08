0962b301d49c01c03b55601d537c92066d387595
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-10-08 21:20:22 +0800

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,41 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
 module.exports = {
+  test_file_extension_regexp: /.*\.ts$/,
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
   networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
     development: {
       host: 'localhost',
       port: 7545,
@@ -9,6 +45,16 @@ module.exports = {
       // from: <address>,        // Account to send txs from (default: accounts[0])
     }
   },
+
+  // this is necessary for coverage
+  coverage: {
+    host: 'localhost',
+    network_id: '*',
+    port: 7545,
+    gas: 0xfffffffffff,
+    gasPrice: 0x01
+  },
+
   // ropsten: {
   //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
   //   network_id: 3,       // Ropsten's id
@@ -17,9 +63,15 @@ module.exports = {
   //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
   //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
   //   },
+
+  // Set default mocha options here, use special reporters etc.
   mocha: {
-    timeout: 100000
+    timeout: 100000,
+    reporter: 'eth-gas-reporter'
   },
+
+  plugins: ['solidity-coverage'],
+
   compilers: {
     solc: {
       version: '0.7.2',
@@ -30,5 +82,8 @@ module.exports = {
         }
       }
     }
+  },
+  ens: {
+    enabled: true
   }
 }
