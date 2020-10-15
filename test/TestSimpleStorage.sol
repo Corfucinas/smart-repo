79e4b323734b8b34a756a151d7d31bfdedc461b5
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-10-15 17:44:50 +0800

    add SimpleStorage test

---
+++ test/TestSimpleStorage.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.7.3;
+
+import 'truffle/Assert.sol';
+import 'truffle/DeployedAddresses.sol';
+import '../contracts/SimpleStorage.sol';
+
+contract TestSimpleStorage {
+    function testItStoresAValue() public {
+        SimpleStorage simpleStorage = SimpleStorage(
+            DeployedAddresses.SimpleStorage()
+        );
+
+        simpleStorage.set(89);
+
+        uint256 expected = 89;
+
+        Assert.equal(
+            simpleStorage.get(),
+            expected,
+            'It should store the value 89.'
+        );
+    }
+}
